50c94beaf49a5ea87bb1e53574103e9566cd1768
dogancanokur <dogancan.okur@gmail.com>
2024-02-04 13:38:24 +0300

    re-init project with Next.js

--- resources/views/form_component/submit.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="form-group">
-    <div class="text-center">
-
-        <button type="submit" class="btn btn-success">
-            <i class="fa fa-btn fa-save"></i> {{ $name }}
-        </button>
-        <a href="{{$url}}" class="btn btn-danger">VAZGEÇ</a>
-    </div>
-</div>
